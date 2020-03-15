fa521489c9425efeedfa86ac4b769c8795cd267c
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-14 21:34:54 -0600

    Style buttons

--- src/App.js
+++ src/App.js
@@ -19,24 +19,11 @@ function App() {
     return (
         <div className="App">
             <h1>The World of Avatar</h1>
-            <div
-                style={{
-                    display: "flex",
-                    width: "300px",
-                    margin: "0 auto",
-                    justifyContent: "space-evenly",
-                    alignItems: "center",
-                    paddingBottom: "1rem"
-                }}
-            >
+            <div className="buttonContainer">
                 <button
+                    className="bookButton"
                     onClick={() => handleClick("book1")}
                     style={{
-                        boxSizing: "border-box",
-                        padding: "5px 10px",
-                        borderRadius: "5px",
-                        fontSize: "1rem",
-                        // color: "lightgray",
                         background:
                             "linear-gradient(cornflowerblue, ghostwhite)"
                     }}
@@ -44,26 +31,18 @@ function App() {
                     Book 1
                 </button>
                 <button
+                    className="bookButton"
                     onClick={() => handleClick("book2")}
                     style={{
-                        boxSizing: "border-box",
-                        padding: "5px 10px",
-                        borderRadius: "5px",
-                        fontSize: "1rem",
-                        // color: "lightgray",
                         background: "linear-gradient(darkgreen, ghostwhite)"
                     }}
                 >
                     Book 2
                 </button>
                 <button
+                    className="bookButton"
                     onClick={() => handleClick("book3")}
                     style={{
-                        boxSizing: "border-box",
-                        padding: "5px 10px",
-                        borderRadius: "5px",
-                        fontSize: "1rem",
-                        // color: "lightgray",
                         background: "linear-gradient(firebrick, ghostwhite)"
                     }}
                 >
